7426088a3ec5a98ae7b607a78d283de924f388cb
SteveMuiyuro <stevemuiyuro@gmail.com>
2023-07-03 14:04:32 +0300

    update build

--- part2/phonebook/src/service/notes.js
+++ part2/phonebook/src/service/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
@@ -16,11 +16,9 @@ const remove = (id, newObj) => {
   return request.then((response) => response.data);
 };
 
-
 const update = (id, newObj) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObj);
-    return request.then((response) => response.data);
-  };
-
+  const request = axios.put(`${baseUrl}/${id}`, newObj);
+  return request.then((response) => response.data);
+};
 
 export default { getAll, create, update, remove };
